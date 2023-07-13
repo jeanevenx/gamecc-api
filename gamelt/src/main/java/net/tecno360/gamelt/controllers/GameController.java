a1c1f5fbd309b1063b398a3174aa6b57ca696a0c
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2023-07-13 11:34:49 -0300

    Get game by id

--- gamelt/src/main/java/net/tecno360/gamelt/controllers/GameController.java
+++ gamelt/src/main/java/net/tecno360/gamelt/controllers/GameController.java
@@ -1,9 +1,11 @@
 package net.tecno360.gamelt.controllers;
 
+import net.tecno360.gamelt.dto.GameDTO;
 import net.tecno360.gamelt.dto.GameMinDTO;
 import net.tecno360.gamelt.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +20,12 @@ public class GameController {
 
     @GetMapping
     public List<GameMinDTO> getAll(){
-        return gameService.findAll();
+        return gameService.getAllGames();
 
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDTO getById(@PathVariable Long id){
+        return gameService.getGameById(id);
     }
 }
--- gamelt/src/main/java/net/tecno360/gamelt/services/GameService.java
+++ gamelt/src/main/java/net/tecno360/gamelt/services/GameService.java
@@ -1,20 +1,33 @@
 package net.tecno360.gamelt.services;
 
+import net.tecno360.gamelt.dto.GameDTO;
 import net.tecno360.gamelt.dto.GameMinDTO;
 import net.tecno360.gamelt.entities.Game;
 import net.tecno360.gamelt.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+import static org.springframework.util.ClassUtils.isPresent;
 
 @Service
 public class GameService {
     @Autowired
     private GameRepository gameRepo;
-    public List<GameMinDTO> findAll(){
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> getAllGames(){
         List<Game> result = gameRepo.findAll();
         return result.stream().map(GameMinDTO::new).toList();
 
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO getGameById(Long id){
+        Game result = gameRepo.findById(id).orElseThrow();
+
+        return new GameDTO(result);
     }
 }
