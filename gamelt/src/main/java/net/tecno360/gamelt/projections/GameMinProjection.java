af15a7c6c81ee78c4995f368b39d0ef81e190c1e
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2023-07-16 17:55:49 -0300

    refactored

--- gamelt/src/main/java/net/tecno360/gamelt/projections/GameMinProjection.java
+++ gamelt/src/main/java/net/tecno360/gamelt/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package net.tecno360.gamelt.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
