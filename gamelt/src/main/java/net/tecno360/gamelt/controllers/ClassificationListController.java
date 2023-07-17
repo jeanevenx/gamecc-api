8c52861cb6d93bf90aa251caf3fd42a47307b545
Jean Evenx Joseph <83151108+jeanevenx@users.noreply.github.com>
2023-07-17 12:56:23 -0300

    change game position in classification

--- gamelt/src/main/java/net/tecno360/gamelt/controllers/ClassificationListController.java
+++ gamelt/src/main/java/net/tecno360/gamelt/controllers/ClassificationListController.java
@@ -1,15 +1,13 @@
 package net.tecno360.gamelt.controllers;
 
+import net.tecno360.gamelt.dto.ChangeGamePositionDTO;
 import net.tecno360.gamelt.dto.ClassificationListDTO;
 import net.tecno360.gamelt.dto.GameDTO;
 import net.tecno360.gamelt.dto.GameMinDTO;
 import net.tecno360.gamelt.services.ClassificationListService;
 import net.tecno360.gamelt.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,5 +32,12 @@ public class ClassificationListController {
 
         return gameService.findGameByClassification(id);
     }
+
+    @PostMapping(value = "/{id}/replacement")
+    public void move(@PathVariable Long id, @RequestBody ChangeGamePositionDTO body){
+
+        classificationListService.move(id, body.getOriginIndex(), body.getDestinationIndex());
+    }
+
 
 }
--- gamelt/src/main/java/net/tecno360/gamelt/services/ClassificationListService.java
+++ gamelt/src/main/java/net/tecno360/gamelt/services/ClassificationListService.java
@@ -1,8 +1,11 @@
 package net.tecno360.gamelt.services;
 
 import net.tecno360.gamelt.dto.ClassificationListDTO;
+import net.tecno360.gamelt.dto.GameMinDTO;
 import net.tecno360.gamelt.entities.ClassificationList;
+import net.tecno360.gamelt.projections.GameMinProjection;
 import net.tecno360.gamelt.repositories.ClassificationListRepository;
+import net.tecno360.gamelt.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,19 +15,37 @@ import java.util.List;
 @Service
 public class ClassificationListService {
     @Autowired
-    private ClassificationListRepository classificationRepo;
+    private ClassificationListRepository classificationListRepo;
+    @Autowired
+    private GameRepository gameRepo;
 
     @Transactional(readOnly = true)
     public List<ClassificationListDTO> findAllClassification(){
-        List<ClassificationList> result = classificationRepo.findAll();
+        List<ClassificationList> result = classificationListRepo.findAll();
         return result.stream().map(ClassificationListDTO::new).toList();
 
     }
 
     @Transactional(readOnly = true)
     public ClassificationListDTO getClassificationById(Long id){
-        ClassificationList result = classificationRepo.findById(id).orElseThrow();
+        ClassificationList result = classificationListRepo.findById(id).orElseThrow();
 
         return new ClassificationListDTO(result);
+    }
+
+    @Transactional(readOnly = true)
+    public void move(Long id, int originIndex, int destinationIndex){
+        List<GameMinProjection> classification = gameRepo.searchByClassification(id);
+        GameMinProjection obj = classification.remove(originIndex);
+        classification.add(destinationIndex, obj);
+
+        int min = Math.min(originIndex, destinationIndex);
+        int max = Math.max(originIndex, destinationIndex);
+
+
+        for(int i=min; i<= max; i++){
+            classificationListRepo.updateBelongingPosition(id, classification.get(i).getId(), i);
+        }
+
     }
 }
